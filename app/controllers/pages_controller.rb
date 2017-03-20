bef43f0ed02698adfe4a020bbf151545b5690359
Aijaz Khan <aijaz-khan@hotmail.com>
2017-03-20 18:18:54 +0500

    diffbot controller added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,19 +4,38 @@ require 'mechanize'
 require 'rubygems'
 require 'nokogiri'
 require 'sanitize'
+require 'uri'
+require 'open-uri'
 
 layout false
 
 def home
 end
 
-def page
-  mechanize = Mechanize.new
-  page = mechanize.get(params[:subject])
-  # Sanitize.clean(page)
-  @a =  page.body
+def home_2
+end
 
+def page
+#   mechanize = Mechanize.new
+# uri = URI.parse(URI.encode(
+str = params[:subject]
+url = ERB::Util.url_encode(str)
+uri = URI('http://api.diffbot.com/v3/analyze?token=d9e198a1c06f26779313bc3aade5270e&timeout=50000&url='+url)
+req = Net::HTTP.get(uri)
+puts req
+  # Sanitize.clean(page)
+  @a =  req
   # render json: a
+end
+
+def scrapper
+str = params[:subject]
+@a = []
+Nokogiri::HTML(open(str)).xpath("//img/@src").each do |src|
+puts src
+@a << src
+end
+
 end
 
 end
